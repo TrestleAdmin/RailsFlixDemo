3e199d60f8ae5d6ec39aa2d74cc1d837515d5998
Sam Pohlenz <sam@sampohlenz.com>
2024-05-27 11:54:46 +0930

    Remove redundant migration

--- db/migrate/20240522065353_add_unique_index_on_people_tmdb_id.rb
+++
@@ -1,8 +0,0 @@
-class AddUniqueIndexOnPeopleTmdbId < ActiveRecord::Migration[7.0]
-  def change
-    # Remove duplicates
-    Person.where(id: Person.select(:id, :tmdb_id).group(:tmdb_id).having("COUNT(*) > 1").pluck("MAX(id)")).delete_all
-
-    add_index :people, :tmdb_id, unique: true
-  end
-end
