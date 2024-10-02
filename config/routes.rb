accf5ea67685fb00a27be1939c879dc7fc09ab5c
Sam Pohlenz <sam@sampohlenz.com>
2024-10-02 20:28:43 +0930

    Add health check route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root to: "home#index"
 
   resources :movies
   resources :tv_shows
